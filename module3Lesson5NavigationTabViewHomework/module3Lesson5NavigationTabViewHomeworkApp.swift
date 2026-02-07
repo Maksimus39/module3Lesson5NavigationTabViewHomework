c8965c9b79e4b74a7c89f2b13a167ff69ea84e71
Maksim Minakov <maksim.minakov.83@mail.ru>
2026-02-07 18:11:00 +0300

    Initial Commit

---
+++ module3Lesson5NavigationTabViewHomework/module3Lesson5NavigationTabViewHomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  module3Lesson5NavigationTabViewHomeworkApp.swift
+//  module3Lesson5NavigationTabViewHomework
+//
+//  Created by Максим Минаков on 07.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct module3Lesson5NavigationTabViewHomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
